e66d8d21f8abde2be7811ba0e14803337c21c246
Collins <mutaic84@gmail.com>
2022-05-07 16:28:22 +0300

    handle liking and deleting a toy

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,35 @@ function App() {
     .then((updatedInfo) => setToys([...toys, updatedInfo]))
   }
 
+  function deleteToy(toyToDelete) {
+    fetch(`http://localhost:3001/toys/${toyToDelete.id}`, {
+      method: "DELETE"
+    })
+    .then((res) => res.json())
+    .then(() => {
+      const remainingToys = toys.filter((toy) => toy.id !== toyToDelete.id)
+      setToys(remainingToys)
+    })
+  }
+
+  function likeToy(likedToy) {
+    fetch(`http://localhost:3001/toys/${likedToy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        ...likedToy,
+        likes: likedToy.likes + 1
+      })
+    })
+    .then((res) => res.json())
+    .then((updatedToy) => {
+      const updatedToys = toys.map((toy) => toy.id === updatedToy.id ? updatedToy : toy)
+      setToys(updatedToys)
+    })
+  }
+
   return (
     <>
       <Header />
@@ -37,7 +66,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer toys={toys}/>
+      <ToyContainer toys={toys} deleteToy={deleteToy} likeToy={likeToy}/>
     </>
   );
 }
