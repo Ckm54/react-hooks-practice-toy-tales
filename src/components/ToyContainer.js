6bc09c874a9e4bdcb5b897975e0948191a58e610
Collins <mutaic84@gmail.com>
2022-05-07 16:28:57 +0300

    pass like and delete handlers as props

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({toys}) {
-  const toyCard = toys.map((toy) => <ToyCard key={toy.id} toy={toy}/>)
+function ToyContainer({toys, deleteToy, likeToy}) {
+  const toyCard = toys.map((toy) => <ToyCard key={toy.id} toy={toy} onToyDelete={deleteToy} onToyLike={likeToy} />)
   return (
     <div id="toy-collection">{ toyCard }</div>
   );
