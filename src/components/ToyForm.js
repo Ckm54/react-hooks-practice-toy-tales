c96d2310ca26aefb067f1c91b32db60b2a07be6e
Collins <mutaic84@gmail.com>
2022-05-07 16:03:28 +0300

    capture form input fields data

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ addToy }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    likes: 0
+  })
+
+  function handleDataInput(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    addToy(formData)
+  }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleFormSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleDataInput}
         />
         <br />
         <input
@@ -17,6 +35,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleDataInput}
         />
         <br />
         <input
